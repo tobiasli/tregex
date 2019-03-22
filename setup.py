8b7272b6326be18b131fac4cf7a22575b369403e
Tobias Litherland <tobiaslland@gmail.com>
2019-03-22 22:18:08 +0100

    add setup for simpler travis

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='tregex',
+      version='1.0',
+      description='Wrapper for more functionality out of regex parse results.',
+      author='Tobias Litherland',
+      url='https://github.com/tobiasli/tregex',
+      packages=['tregex'],
+      )
