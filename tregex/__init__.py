7288b466c1169471de34d53f341b7732387eb090
Tobias Litherland <tobiaslland@gmail.com>
2019-03-24 21:26:58 +0100

    Removed code examples from doc, and referenced github instead

--- tregex/__init__.py
+++ tregex/__init__.py
@@ -1,19 +1,6 @@
 """tregex is a package for handling regular expressions and other string-related tasks more smoothly.
 
->>> import tregex as tr
->>>
->>> t = tr.to_tuple(pattern='([^;]+?)@(.+?)\.([^;]+)', string='john.smith@somewhere.co.uk; hackzor@coolface.com')
->>> assert t[0][1] == 'somewhere'
->>> assert t[1][2] == 'com'
->>>
->>> pattern = '(?P<name>[^;]+?)@(?P<address>.+?)\.(?P<domain>[^;]+)'
->>> t = tr.to_dict(pattern=pattern, string='john.smith@somewhere.co.uk; hackzor@coolface.com')
->>> assert t[0]['name'] == 'john.smith'
->>> assert t[1]['address'] == 'coolface'
->>>
->>> t = tr.to_object(pattern=pattern, string='john.smith@somewhere.co.uk; hackzor@coolface.com')
->>> assert t[0].name == 'john.smith'
->>> assert t[1].address == 'coolface'
+For usage examples, see https://github.com/tobiasli/tregex
 """
 from tregex.tregex import TregexCompiled
 from tregex.tregex import to_tuple, to_dict, to_object
