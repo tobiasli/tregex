51235f201abbf5b0b96547e0927ee379dee6a835
Tobias Litherland <tobiaslland@gmail.com>
2019-11-16 22:12:33 +0100

    Some methods where missing from tregex/__init__.py

--- tregex/__init__.py
+++ tregex/__init__.py
@@ -3,7 +3,7 @@
 For usage examples, see https://github.com/tobiasli/tregex
 """
 from tregex.tregex import TregexCompiled
-from tregex.tregex import to_tuple, to_dict, to_object
+from tregex.tregex import to_tuple, to_dict, to_object, match, smart
 from tregex.tregex import similarity, find, find_best, find_scores
 
-__all__ = ['TregexCompiled', 'to_tuple', 'to_dict', 'to_object', 'similarity', 'find', 'find_best', 'find_scores']
+__all__ = ['TregexCompiled', 'to_tuple', 'to_dict', 'to_object', 'similarity', 'find', 'find_best', 'find_scores', 'match', 'smart']
